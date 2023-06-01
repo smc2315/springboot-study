913b310e773a6315f7884989572d4e3f8f278592
smc2315 <choeyongtae085@gmail.com>
2023-06-01 17:26:23 +0900

    feat: MemberController 추가

---
+++ src/main/java/com/example/demo/member/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.example.demo.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+}
