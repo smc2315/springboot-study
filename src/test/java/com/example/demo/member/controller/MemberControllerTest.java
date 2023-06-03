f6f4aad7fcc4deefd07d64ff00df2259b5a9b7af
smc2315 <choeyongtae085@gmail.com>
2023-06-03 15:21:51 +0900

    feat: member 등록 fail test 추가

--- src/test/java/com/example/demo/member/controller/MemberControllerTest.java
+++ src/test/java/com/example/demo/member/controller/MemberControllerTest.java
@@ -2,6 +2,7 @@ package com.example.demo.member.controller;
 
 import com.example.demo.ApiDocument;
 import com.example.demo.member.dto.MemberRequest;
+import com.example.demo.member.dto.MemberResponse;
 import com.example.demo.member.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.BDDMockito.willThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -24,6 +26,8 @@ public class MemberControllerTest extends ApiDocument {
     private static final String FAIL_MESSAGE = "fail";
 
     private MemberRequest memberRequest;
+    private MemberResponse successResponse;
+    private MemberResponse failResponse;
 
     @MockBean
     private MemberService memberService;
@@ -34,19 +38,40 @@ public class MemberControllerTest extends ApiDocument {
                 .name("최용태")
                 .age(27)
                 .build();
+        successResponse = MemberResponse.builder()
+                .message(SUCCESS_MESSAGE)
+                .build();
+        failResponse = MemberResponse.builder()
+                .message(FAIL_MESSAGE)
+                .build();
     }
     @Test
     public void create_member_success() throws Exception{
         //given
-        willReturn(1L).given(memberService).create(any(MemberRequest.class));
+        willReturn(successResponse).given(memberService).create(any(MemberRequest.class));
         //when
         ResultActions resultActions = mockMvc.perform(post("/api/v1/members")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(toJson(memberRequest)));
         //then
         resultActions.andExpect(status().isOk())
-                .andExpect(content().string("1"))
+                .andExpect(content().json(toJson(successResponse)))
                 .andDo(print())
                 .andDo(toDocument("create-member-success"));
      }
+
+     @Test
+     public void create_member_fail() throws Exception{
+         //given
+         willThrow(new IllegalArgumentException(FAIL_MESSAGE)).given(memberService).create(any(MemberRequest.class));
+         //when
+         ResultActions resultActions = mockMvc.perform(post("/api/v1/members")
+                 .contentType(MediaType.APPLICATION_JSON)
+                 .content(toJson(memberRequest)));
+         //then
+         resultActions.andExpect(status().isBadRequest())
+                 .andExpect(content().json(toJson(failResponse)))
+                 .andDo(print())
+                 .andDo(toDocument("create-member-fail"));
+      }
 }
