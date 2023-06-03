ae3aa822bbe6cdf32d19a22a2214e22f0a032177
smc2315 <choeyongtae085@gmail.com>
2023-06-03 15:20:28 +0900

    feat: ControllerAdvice 추가

---
+++ src/main/java/com/example/demo/member/controller/ControllerAdvice.java
@@ -0,0 +1,15 @@
+package com.example.demo.member.controller;
+
+import com.example.demo.member.dto.MemberResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<MemberResponse> runtimeException() {
+        return ResponseEntity.badRequest().body(MemberResponse.builder().message("fail").build());
+    }
+}
