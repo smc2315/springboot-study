4c495b708d39ed8172e925dc41b5b90973030157
smc2315 <choeyongtae085@gmail.com>
2023-06-05 14:22:49 +0900

    feat: PostResponse 변수 추가

--- src/main/java/com/example/demo/post/dto/PostResponse.java
+++ src/main/java/com/example/demo/post/dto/PostResponse.java
@@ -1,4 +1,15 @@
 package com.example.demo.post.dto;
 
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private String writer;
+    private int viewCnt;
 }
