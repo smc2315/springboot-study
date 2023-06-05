e20acd39f9155c634580097fb9c3b9b01d2f9221
smc2315 <choeyongtae085@gmail.com>
2023-06-05 19:09:12 +0900

    feat: post create 추가

--- src/main/java/com/example/demo/post/service/PostService.java
+++ src/main/java/com/example/demo/post/service/PostService.java
@@ -1,4 +1,9 @@
 package com.example.demo.post.service;
 
+import com.example.demo.post.controller.dto.PostRequest;
+
 public class PostService {
+    public Long create(PostRequest postRequest) {
+        return null;
+    }
 }
