57b5b30bfe6557a9764f202e03f6ead5387530a4
smc2315 <choeyongtae085@gmail.com>
2023-06-05 13:50:21 +0900

    feat: PostController 추가

---
+++ src/main/java/com/example/demo/post/controller/PostController.java
@@ -0,0 +1,11 @@
+package com.example.demo.post.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
+public class PostController {
+}
