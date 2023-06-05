ee1e28965173a53490d1309b031c2805608084e2
smc2315 <choeyongtae085@gmail.com>
2023-06-05 13:53:57 +0900

    feat: PostRequest 변수 추가

--- src/main/java/com/example/demo/post/dto/PostRequest.java
+++ src/main/java/com/example/demo/post/dto/PostRequest.java
@@ -1,4 +1,15 @@
 package com.example.demo.post.dto;
 
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostRequest {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String writer;
 }
