a96e6bfc6b451ce83617d2ceea3c1a5b73d369aa
smc2315 <choeyongtae085@gmail.com>
2023-06-05 19:08:27 +0900

    feat: post create 추가

--- src/main/java/com/example/demo/post/controller/PostController.java
+++ src/main/java/com/example/demo/post/controller/PostController.java
@@ -1,6 +1,11 @@
 package com.example.demo.post.controller;
 
+import com.example.demo.post.controller.dto.PostRequest;
+import com.example.demo.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<Long> create(@RequestBody PostRequest PostRequest) {
+        return ResponseEntity.ok(postService.create(PostRequest));
+    }
 }
