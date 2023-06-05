d45e2f6ee8ec56dc8aac8cd40cd08a4946500a9b
smc2315 <choeyongtae085@gmail.com>
2023-06-05 19:55:15 +0900

    refactor: @Service 추가

--- src/main/java/com/example/demo/post/service/PostService.java
+++ src/main/java/com/example/demo/post/service/PostService.java
@@ -1,7 +1,9 @@
 package com.example.demo.post.service;
 
 import com.example.demo.post.controller.dto.PostRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PostService {
     public Long create(PostRequest postRequest) {
         return null;
