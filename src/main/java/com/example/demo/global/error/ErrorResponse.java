640cf22ee89d06b6204e94f3d2a3544825a55987
smc2315 <choeyongtae085@gmail.com>
2023-06-05 19:53:37 +0900

    feat: ErrorResponse 추가

---
+++ src/main/java/com/example/demo/global/error/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.example.demo.global.error;
+
+import com.example.demo.global.error.exception.ErrorCode;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+
+    private String message;
+    private String code;
+
+    private ErrorResponse(final ErrorCode code) {
+        this.message = code.getMessage();
+        this.code = code.getCode();
+    }
+    public static ErrorResponse of(final ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+}
