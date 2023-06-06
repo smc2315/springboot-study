937021936a149382a28f21094e4f4ca68fc742ed
smc2315 <choeyongtae085@gmail.com>
2023-06-06 15:46:16 +0900

    feat: NotNull 추가

--- src/main/java/com/example/demo/post/domain/Post.java
+++ src/main/java/com/example/demo/post/domain/Post.java
@@ -1,20 +1,32 @@
 package com.example.demo.post.domain;
 
+import com.sun.istack.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Post {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private String title;
 
+    @NotNull
     private String content;
 
+    @NotNull
     private String writer;
 
 }
