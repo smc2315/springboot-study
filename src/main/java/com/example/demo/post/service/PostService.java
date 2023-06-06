7435f75b3e8145056d91f9b5edc4dcba15d0b235
smc2315 <choeyongtae085@gmail.com>
2023-06-06 16:07:49 +0900

    feat: create 메서드 구현

--- src/main/java/com/example/demo/post/service/PostService.java
+++ src/main/java/com/example/demo/post/service/PostService.java
@@ -4,23 +4,35 @@ import com.example.demo.post.controller.dto.PostRequest;
 import com.example.demo.post.controller.dto.PostResponse;
 import com.example.demo.post.controller.dto.PostUpdateRequest;
 import com.example.demo.post.controller.dto.PostsResponse;
+import com.example.demo.post.domain.Post;
+import com.example.demo.post.domain.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class PostService {
+
+    private final PostRepository postRepository;
+
     public Long create(PostRequest postRequest) {
-        return null;
+
+        Post post = postRequest.toEntity();
+        return postRepository.save(post).getId();
     }
 
     public PostResponse getPost(long anyLong) {
+
         return null;
     }
 
     public PostsResponse getPosts() {
+
         return null;
     }
 
     public Long update(Long postId,PostUpdateRequest postUpdateRequest) {
+
         return null;
     }
 
