ba97477b8863ab8864cb1c506012b4ab4b011bd4
smc2315 <choeyongtae085@gmail.com>
2023-06-06 15:47:10 +0900

    feat: toEntity 추가

--- src/main/java/com/example/demo/post/controller/dto/PostRequest.java
+++ src/main/java/com/example/demo/post/controller/dto/PostRequest.java
@@ -1,5 +1,6 @@
 package com.example.demo.post.controller.dto;
 
+import com.example.demo.post.domain.Post;
 import lombok.*;
 
 import javax.persistence.GeneratedValue;
@@ -10,7 +11,16 @@ import javax.persistence.Id;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostRequest {
+
     private String title;
     private String content;
     private String writer;
+
+    public Post toEntity() {
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .writer(writer)
+                .build();
+    }
 }
