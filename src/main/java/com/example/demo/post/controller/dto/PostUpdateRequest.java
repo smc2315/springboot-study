8a79f8514ceef060e6634214846e46393941a640
smc2315 <choeyongtae085@gmail.com>
2023-06-06 14:52:05 +0900

    feat: PostUpdateRequest 추가

---
+++ src/main/java/com/example/demo/post/controller/dto/PostUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.example.demo.post.controller.dto;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostUpdateRequest {
+    private String title;
+    private String content;
+}
